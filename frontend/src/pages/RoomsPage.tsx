b64886308a562ed0a1100b4611721252f40a5763
Ashish Ranjan Singh <singhashishranjan362@gmail.com>
2026-08-29 23:31:28 +0530

    style: increase width and height dimensions of name logo image across all pages

--- frontend/src/pages/RoomsPage.tsx
+++ frontend/src/pages/RoomsPage.tsx
@@ -77,7 +77,7 @@ const RoomsPage: React.FC = () => {
         <div className="h-16 flex items-center justify-between px-4 shrink-0 border-b border-[#28292d]">
           <div className="flex items-center gap-3 cursor-pointer" onClick={() => navigate('/dashboard')}>
             <img src="/chat.png" alt="Logo" className="w-8 h-8 object-contain" />
-            <img src="/name.png" alt="SyncStream" className="h-6 w-auto object-contain" />
+            <img src="/name.png" alt="SyncStream" className="h-7 w-32 object-contain" />
           </div>
           <button 
             onClick={() => setShowMobileSidebar(false)}
